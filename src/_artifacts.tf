57b0b2ed4a03ccecbe43f2c2cd8b1439b1f405f2
chrisghill <chris@massdriver.cloud>
2022-08-09 01:51:07 -0600

    add artifact

--- src/_artifacts.tf
+++ src/_artifacts.tf
@@ -1,18 +1,29 @@
-# resource "massdriver_artifact" "<name>" {
-#   field                = "the field in the artifacts schema"
-#   provider_resource_id = "AWS ARN or K8S SelfLink"
-#   type                 = "file-name-from-artifacts"
-#   name                 = "a contextual name for the artifact"
-#   artifact = jsonencode(
-#     {
-#       # data = {
-#       #   # This should match the aws-rds-arn.json schema file
-#       #   arn = "aws::..."
-#       # }
-#       # specs = {
-#       #   # Any existing spec in ./specs
-#       #   # aws = {}
-#       # }
-#     }
-#   )
-# }
+resource "massdriver_artifact" "sftp" {
+  field                = "sftp"
+  provider_resource_id = aws_transfer_server.main.arn
+  name                 = "AWS Transfer Family SFTP Server: ${aws_transfer_server.main.arn}"
+  artifact = jsonencode(
+    {
+      data = {
+        infrastructure = {
+          arn = aws_transfer_server.main.arn
+        }
+        authentication = {
+          hostname    = aws_transfer_server.main.endpoint
+          user        = aws_transfer_user.main.user_name
+          public_key  = tls_private_key.main.public_key_pem
+          private_key = tls_private_key.main.private_key_pem
+        }
+      }
+      specs = {
+        aws = {
+          region = local.region
+        }
+        cryptography = {
+          algorithm = tls_private_key.main.algorithm
+          key_size  = tls_private_key.main.rsa_bits
+        }
+      }
+    }
+  )
+}
